1b9c24c300471961eaf2810f7386b48f99ece400
Azacool <aziztukhtasinov0404@gmail.com>
2023-11-15 01:58:50 +0500

    blog/post project

--- post/views.py
+++ post/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from .models import Post
 
 def main(request):
-    #content1={'content': [{'id':'2','title':'giving help','date':'10/12/2019'}]}
-    content=Post.objects.all()
+    content={'content': [{'id':'2','title':'Giving Help','date':'10/12/2019','image':'images/giving_help.jpg'}]}
     return render(request,template_name='index.html',context=content)
 
 
